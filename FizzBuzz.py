420c76a96ec7a1917224576be79874f54abdc51a
Oscar S <ols6vk@umsystem.edu>
2023-03-06 14:01:14 -0600

    file updated

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,4 @@
 number = 0
 while(number < 100):
     number+=1
-    if number%3==0:
-        print(number,"Fizz")
-    if number%5==0:
-        print(number,"Buzz")
-    else:
-        print(number)
+    print(number)
