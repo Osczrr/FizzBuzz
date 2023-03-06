ff48c602e5ed50fc3b957128238d34bfb308917b
Oscar <89543006+Osczrr@users.noreply.github.com>
2023-03-06 13:53:49 -0600

    Delete FizzBuzz1.py

--- FizzBuzz1.py
+++
@@ -1,9 +0,0 @@
-number = 0
-while(number < 100):
-    number+=1
-    if number%3==0:
-        print(number,"Fizz")
-    if number%5==0:
-        print(number,"Buzz")
-    else:
-        print(number)
