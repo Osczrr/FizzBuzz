3580e5e715dace666d523f4a86113a86313c3f48
Oscar S <ols6vk@umsystem.edu>
2023-03-06 14:13:14 -0600

    Finished FizzBuzz Updated to main branch

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,11 @@
 number = 0
 while(number < 100):
     number+=1
-    if number%3==0:
-        print(number,"Fizz")
-    if number%5==0:
-        print(number,"Buzz")
-    if number %5==0 and number%3==0:
+    if number%3==0 and number%5==0:
         print(number,"FizzBuzz")
+    elif number%3==0:
+        print(number,"Fizz")
+    elif number %5==0:
+        print(number,"Buzz")
     else:
         print(number)
